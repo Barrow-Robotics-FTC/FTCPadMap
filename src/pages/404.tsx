8dcccae1d479fceecdf2a13b2a1a5f0b85dfa64a
DylDev <dylanb92010@gmail.com>
2025-10-06 19:35:07 -0400

    404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { useRouter } from "next/router"
+import { Button } from "@/components/ui/button"
 
 export default function NotFound() {
+    const router = useRouter()
     return (
-        <div>
-            <h1 className="text-2xl">404</h1>
+        <div className="flex h-screen flex-col items-center justify-center">
+            <h1>FTCPadMap - 404</h1>
+            <p className="text-sm text-zinc-500 mt-3">Why are we here..... on this spinning rock.....</p>
+            <Button onClick={() => router.push("/")} className="mt-6">Go Home</Button>
         </div>
-    );
+    )
 }
